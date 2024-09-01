58a644a80e8fd7cb3007761f656b069daccfb4ac
Dilon Sok <sokdilon@gmail.com>
2024-09-01 15:01:06 -0500

    .

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 $(function() {
     console.log("Main Init Called");
     init();
@@ -26,7 +27,40 @@ function InitFilesRanksBrd(){
     console.log("FilesBrd[SQUARES.A1]: " + FilesBrd[SQUARES.A1] + " RanksBrd[SQUARES.A1]: " + RanksBrd[SQUARES.A1]);
     console.log("FilesBrd[SQUARES.E8]: " + FilesBrd[SQUARES.E8] + " RanksBrd[SQUARES.E8]: " + RanksBrd[SQUARES.E8]);
 }
+
+//creating unique keys for position/helps with engine analysis
+function InitHashKeys(){
+    let index = 0;
+
+    for(index = 0; index < 14 * 120; index++){
+        PieceKeys[index] = RAND_32();
+    }
+    sideKey = RAND_32;
+
+    for(index = 0; index < 16; index ++){
+        castleKeys[index] = RAND_32();
+    }
+}
+
+function InitSq120To64(){
+    let index = 0;
+    let file = FILES.FILE_A;
+    let rank = RANKS.RANK_1;
+    let sq = SQUARES.A1;
+    let sq64 = 0;
+
+    for(to64 in Sq120ToSq64){
+        Sq120ToSq64[to64] = 65; //arbitrary number
+    }
+
+    for(to120 in Sq64ToSq120){
+        Sq64ToSq120[to120] = 120; //arbitary number
+    }
+    
+}
+
 function init(){
     console.log("init called");
     InitFilesRanksBrd();
+    InitHashKeys();
 }
